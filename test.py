5d644a1d3b0fcfe6313e4d1066dcc2f83abe7494
Joel Torres <jt@joeltorres.org>
2025-06-10 22:32:08 -0500

    Init project files

---
+++ test.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 Joel Torres
+# Distributed under the MIT License. See the accompanying file LICENSE.
+
+import unittest
+import os
+
+class TestBrpcli(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
