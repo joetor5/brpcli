37b12fab2d0df8b96b89c062872aece4150d31ba
Joel Torres <jt@joeltorres.org>
2025-06-10 22:52:48 -0500

    Add pip requirements

--- brpcli.sh
+++ brpcli.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# Copyright (c) 2025 Joel Torres
+# Distributed under the MIT License. See the accompanying file LICENSE.
 
---
+++ brpcli/cli.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 Joel Torres
+# Distributed under the MIT License. See the accompanying file LICENSE.
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
